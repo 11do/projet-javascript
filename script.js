2b1d36b8ad90d966c57955ef6451d6d3771f95a2
11do <nanadorinelle@gmail.com>
2023-09-06 00:32:20 +0200

    adding constructor and selector

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const listeMots = ["Cachalot","Petunia","Serviette"]
 let score=0
+for(let i = 0; i < 3; i++){
+  let mot = prompt("entre le mot " + listeMots[i])
 
-let mot = prompt("entre le mot " + listeMots[0])
-
-if(mot=== listeMots[0]){
+  if(mot=== listeMots[i]){
     score+=1
+  }
+
 }
+console.log(score)
 
-let secondMot = prompt("entrez le second mot " + listeMots[1])
-switch(secondMot){
-    case listeMots[1]:
-        score+=1
-        break
+let listePhrases = ["pas de panique!","La vie, l'univers et le reste","Merci pour le poisson"]
+
+let mot = prompt("veuillez entrer la liste de mot que vous voulez "+ "mots ou phrases")
+while(mot!== "mots" && mot !== "phrases"){
+    mot = prompt("veuillez entrer la liste de mot que vous voulez "+ "mots ou phrases")  
 }
+if (mot === "mots"){
+    for(let i = 0; i < 3; i++){
+        let mot1 = prompt("entre le mot " + listeMots[i])
+      
+        if(mot1=== listeMots[i]){
+            score+=1
+          
+        }
+    
+    }
+
+}else{
+     
+    for(let j = 0; j < 3; j++){
+        let mot2 = prompt("entre le mot " + listePhrases[j])
+      
+        if(mot2=== listePhrases[j]){
+          score+=1
+        }    
+    }   
+    
 
 
-let thirdMot =  prompt("entrez le second mot " + listeMots[2])
-switch(thirdMot){
-    case listeMots[2]:
-        score+=1
-        break
+     
 }
-
 console.log(score)
